d808a46e7232627d5eeb0ef8d841333554a5e611
amritaarora0902 <amritaarora2424@gmail.com>
2021-09-08 16:44:15 +0530

    Header-material ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Grid from '@material-ui/core/Grid';
+import Header from './components/Header';
+import List from './components/List';
+import Map from './components/Map';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+
+    <Grid container spacing ={3}>
+      <Grid item xs={6} md={4}>
+          <List />
+      </Grid>
+      <Grid item xs={6} md={8}> 
+          <Map />
+      </Grid>
+
+    </Grid>
+    </>
   );
 }
 
