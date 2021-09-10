fd457e2ff89589b67e2edd9226e52f75ebf7f734
amritaarora0902 <amritaarora2424@gmail.com>
2021-09-10 22:31:49 +0530

    google-map-react integration

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  mapContainer: {
+    height: '75vh', width: '100%',
+    }
+    
+}));
+
 
 const Map = () => {
+    const classes = useStyles();
+    const coordinates = {lat:0, lng:0};
+
     return (
-        <div>
-            <h1>Map</h1>
+        <div className={classes.mapContainer}>
+             <GoogleMapReact
+            bootstrapURLKeys={{ key: 'AIzaSyAgHVLlOlIvuig21I1jc49Ia1IHPKJE3aU' }}
+            defaultCenter={coordinates}
+            center={coordinates}
+            defaultZoom={14}
+            margin={[50,50,50,50]}
+            options={''}
+            onChange={''}
+            onChildClick={''}
+             >  
+
+
+        </GoogleMapReact> 
         </div>
-    )
+    );
 }
 
 export default Map;
